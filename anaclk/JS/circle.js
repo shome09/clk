83ed9ca38a97482b69ebb30a677ad88a4718fc8d
debjit shome <debjit.shome99@gmail.com>
2024-01-01 23:33:43 +0530

    clk

---
+++ anaclk/JS/circle.js
@@ -0,0 +1,10 @@
+setInterval(()=>{
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    //console.log(h+" "+m+" "+s)
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
+})
